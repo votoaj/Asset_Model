4baf61043fce15021de2277bbf260cc2ceb576e4
Anthony Voto <88912730+votoaj@users.noreply.github.com>
2022-08-22 07:11:40 -0400

    Update assetmodel.c

--- assetmodel.c
+++ assetmodel.c
@@ -234,7 +234,7 @@ int bisectionSearch(float arr[], int lo, int hi, float x)
 
 
 
-float bond_mkt_value(float coupon, float prin, int pmt_freq, int periods,float treasury_rate, float mkt_sprd)
+float market_value_bond(float coupon, float prin, int pmt_freq, int periods,float treasury_rate, float mkt_sprd)
 {
 	float mkt_value = 0.0;
 	float v = 1.0;
@@ -280,10 +280,48 @@ float bond_mkt_value(float coupon, float prin, int pmt_freq, int periods,float t
 	*/
 }
 
+void cashflow_bond(float buf[1000],float scn[361][10], float coupon, float prin, int pmt_freq, int periods)
+{
+	int time_step = 0;
+	coupon /= pmt_freq;
+		
+	while (periods > 0)
+	{
+		if (periods % (12 / pmt_freq) == 0)
+		{
+			buf[time_step] = prin * coupon;
+		}
+		
+		time_step++;
+		periods--;
+		
+	}
+	
+	buf[time_step] = prin * (1 + coupon);
+}
 
 int main()
 {
-
+	
+	int row = 0;
+	float scn[361][10] = {{0,0}};
+	char line[1000];
+	char* token = NULL;
+	FILE* fp = fopen("C:\\work\\C\\assetmodel\\scn.csv", "r");
+	
+	float temp = 0.0;
+	
+	for (int i = 0; i < 361; i++)
+	{
+		int col = 0;
+		
+		while ((fscanf(fp, "%f", &scn[i][col++]) == 1) && col < 10)
+		{
+			fscanf(fp, ",");
+		}
+	}
+	
+		
 	int periods = 21;
 	float mkt_rate = 0.0545;
 	float treasury = 0.0;
@@ -294,16 +332,17 @@ int main()
 	
 	float yc_tenors[10] = { 0.25, 0.5, 1, 2, 3, 5, 7, 10, 20, 30 };
 	
-	float scn[6][10] = {
+	/*float scn[6][10] = {
 					{0.0006,	0.0006,	0.0019,	0.0039,	0.0073,	0.0097,	0.0126,	0.0144,	0.0152,	0.0194},
 					{0.0044,	0.0046,	0.0059,	0.0079,	0.0112,	0.0134,	0.0163,	0.0181,	0.0189,	0.023},
 					{0.0082,	0.0085,	0.01,	0.0119,	0.015,	0.0172,	0.0199,	0.0217,	0.0226,	0.0265},
 					{0.0121,	0.0125,	0.014,	0.0159,	0.0189,	0.0209,	0.0236,	0.0254,	0.0263,	0.0301},
 					{0.0159,	0.0165,	0.018,	0.0199,	0.0227,	0.0246,	0.0272,	0.029,	0.03,	0.0336},
 					{0.0197,	0.0205,	0.0221,	0.024,	0.0266,	0.0284,	0.0309,	0.0327,	0.0337,	0.0372}
-					};
-					
-					
+					};*/
+	
+	
+	
 	float rates[10] = {0.0006,	0.0006,	0.0019,	0.0039,	0.0073,	0.0097,	0.0126,	0.0144,	0.0152,	0.0194};
 	
 	
@@ -325,11 +364,16 @@ int main()
 	
 	printf("%d\n", periods);
 	
-	float hiImFrack = powf(0.95, 0.5);
+	float cashflows[1000] = {0};
 	
-	printf("%f\n", hiImFrack);
+	cashflow_bond(cashflows, scn, 0.053, 2000000.0, 2, periods);
 	
-	mkt_value = bond_mkt_value(	0.053, 2000000.0, 1,periods, 0.03, 0.0245);
+	for (int i = 0; i < 364; i++)
+	{
+		printf("%f\n", cashflows[i]);
+	}
+
+	mkt_value = market_value_bond(	0.053, 2000000.0, 1,periods, 0.03, 0.0245);
 	
 	printf("%f\n", mkt_value);
 	
