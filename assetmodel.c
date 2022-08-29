c1e527471a541edfdc0dae8afb71c30312f690e3
Anthony Voto <88912730+votoaj@users.noreply.github.com>
2022-08-29 17:29:33 -0400

    Update assetmodel.c

--- assetmodel.c
+++ assetmodel.c
@@ -9,7 +9,32 @@ struct Date {
 };
 
 
+/*
+float fmax(float x, float y)
+{
+	if (x > y)
+	{
+		return x;
+	} 
+	else
+	{
+		return y;
+	}
+}
 
+
+float fmin(float x, float y)
+{
+	if (x < y)
+	{
+		return x;
+	}
+	else
+	{
+		return y;
+	}
+}
+*/
 
 int monthDiff(struct Date date1, struct Date date2)
 {
@@ -48,12 +73,10 @@ float dateDiff(int day1, int mon1, int year1, int day2, int mon2, int year2)
 	
 	int JDN1 = 367 * year1 - (7 * (year1 + 5001 + (mon1 - 9) / 7)) / 4 + (275 * mon1) / 9 + day1 + 1729777;
 	int JDN2 = 367 * year2 - (7 * (year2 + 5001 + (mon2 - 9) / 7)) / 4 + (275 * mon2) / 9 + day2 + 1729777;
-
 	result = ((float)JDN2 - (float)JDN1) / 365.25;
 	
 	return result;
 }
-
 */
 
 
@@ -284,7 +307,7 @@ void cashflow_bond(float buf[1000],float scn[361][10], float coupon, float prin,
 }
 
 
-float market_value_commercial_mortgage(float coupon, float prin, int pmt_freq, int periods,float treasury_rate, float mkt_sprd, int amort_term)
+float market_value_mortgage(float coupon, float prin, int pmt_freq, int periods,float treasury_rate, float mkt_sprd, int amort_term)
 {
 	float mkt_value = 0.0;
 	float v = 1.0;
@@ -319,12 +342,111 @@ float market_value_commercial_mortgage(float coupon, float prin, int pmt_freq, i
 }
 
 
+float cashflow_mortgage(float scn[361][10], float coupon, float prin, int pmt_freq, int periods, int amort_term)
+{
+	int time_step = 0;
+	coupon /= pmt_freq;
+	float payment = prin / ((1.0 - powf( 1.0 / ( 1.0 + coupon) , amort_term)) / coupon);
+	
+	float cashflow = 0.0;
+		
+
+	if (periods % (12 / pmt_freq) == 0 && periods > 0)
+	{
+		cashflow = payment;
+	}
+	else if (periods == 0)
+	{
+		cashflow = prin;
+	}
+
+	
+	return cashflow;
+
+}
+
+
+
+float market_value_cmo(float cmoAPortion, float cmoASpread, float cmoBPortion, float cmoBSpread, float cmoCSpread, float underlying_coupon, float underlying_prin, int pmt_freq, int periods,float treasury_rate, float mkt_sprd, float default_rate)
+{
+	float mkt_value = 0.0;
+	float v = 1.0;
+	float mkt_rate = (treasury_rate + mkt_sprd) / 12.0;
+	underlying_coupon /= pmt_freq;
+	float interest = 0.0;
+	float payment = 0.0;
+	
+	float cmoAPrin = underlying_prin * cmoAPortion;
+	float cmoBPrin = underlying_prin * cmoBPortion;
+	float cmoCPrin = underlying_prin * (1.0 - cmoAPortion - cmoBPortion);
+
+	float cmoAIntEarned = 0.0;
+	float cmoBIntEarned = 0.0;
+	float cmoCIntEarned = 0.0;
+
+	float cmoAIntPaid = 0.0;
+	float cmoBIntPaid = 0.0;
+	float cmoCIntPaid = 0.0;
+
+	float cmoAPrinPaid = 0.0;
+	float cmoBPrinPaid = 0.0;
+	float cmoCPrinPaid = 0.0;
+	
+	while (periods > 0)
+	{
+		
+		if (periods % (12 / pmt_freq) == 0)
+		{
+			
+			underlying_prin *= powf((1 - default_rate), 1.0 / pmt_freq);
+			
+			payment = underlying_prin / ((1.0 - powf( 1.0 / ( 1.0 + underlying_coupon) , periods)) / underlying_coupon);
+			
+			interest = underlying_prin * underlying_coupon;
+			
+			cmoAIntEarned = cmoAPrin * (underlying_coupon + cmoASpread / pmt_freq);
+			cmoBIntEarned = cmoBPrin * (underlying_coupon + cmoBSpread / pmt_freq);
+			cmoCIntEarned = cmoCPrin * (underlying_coupon + cmoCSpread / pmt_freq);
+			
+			cmoAIntPaid = fmin(cmoAIntEarned, payment);
+			cmoBIntPaid = fmin(cmoBIntEarned, payment - cmoAIntPaid);
+			cmoCIntPaid = fmin(cmoCIntEarned, payment - cmoAIntPaid - cmoBIntPaid);
+			
+			
+			cmoAPrinPaid = fmax(fmin(cmoAPrin, payment - cmoAIntPaid - cmoBIntPaid - cmoCIntPaid), 0.0);
+			cmoAPrin -= cmoAPrinPaid;
+			
+			cmoBPrinPaid = fmax(fmin(cmoBPrin, payment - cmoAIntPaid - cmoBIntPaid - cmoCIntPaid - cmoAPrinPaid), 0.0);
+			cmoBPrin -= cmoBPrinPaid;
+			
+			cmoCPrinPaid = fmax(fmin(cmoCPrin, payment - cmoAIntPaid - cmoBIntPaid - cmoCIntPaid - cmoAPrinPaid - cmoBPrinPaid), 0.0);
+			cmoCPrin -= cmoCPrinPaid;
+			
+			//printf("Prin & Interest paid at period %d: %f, %f, %f, %f, %f\n", periods, cmoBPrinPaid, cmoBIntPaid,cmoAPrinPaid, cmoAIntPaid, cmoCIntPaid);
+			
+			mkt_value += (cmoBPrinPaid + cmoBIntPaid) * v * powf(1.0 / (1 + mkt_rate), 0.5);  // powf is to account for timings, we assume payments are mid month
+			
+			underlying_prin = underlying_prin - payment + interest;
+		}
+		
+		v /= (1 + mkt_rate);
+		
+		periods--;
+	}
+	
+	return mkt_value;
+	
+}
+
+
+
 
 
 
 
 int main()
 {
+
 	
 	int row = 0;
 	float scn[361][10] = {{0,0}};
@@ -344,40 +466,18 @@ int main()
 		}
 	}
 	
-		
+	
 	int periods = 21;
-	float mkt_rate = 0.0545;
 	float treasury = 0.0;
 	float mkt_value = 0.0;
-
-	
-	float proj_yrs = 5;
 	
 	float yc_tenors[10] = { 0.25, 0.5, 1, 2, 3, 5, 7, 10, 20, 30 };
-	
-	/*float scn[6][10] = {
-					{0.0006,	0.0006,	0.0019,	0.0039,	0.0073,	0.0097,	0.0126,	0.0144,	0.0152,	0.0194},
-					{0.0044,	0.0046,	0.0059,	0.0079,	0.0112,	0.0134,	0.0163,	0.0181,	0.0189,	0.023},
-					{0.0082,	0.0085,	0.01,	0.0119,	0.015,	0.0172,	0.0199,	0.0217,	0.0226,	0.0265},
-					{0.0121,	0.0125,	0.014,	0.0159,	0.0189,	0.0209,	0.0236,	0.0254,	0.0263,	0.0301},
-					{0.0159,	0.0165,	0.018,	0.0199,	0.0227,	0.0246,	0.0272,	0.029,	0.03,	0.0336},
-					{0.0197,	0.0205,	0.0221,	0.024,	0.0266,	0.0284,	0.0309,	0.0327,	0.0337,	0.0372}
-					};*/
-	
-	
-	
-	float rates[10] = {0.0006,	0.0006,	0.0019,	0.0039,	0.0073,	0.0097,	0.0126,	0.0144,	0.0152,	0.0194};
-	
-	
-
+		
 	int len = sizeof(yc_tenors) / sizeof(yc_tenors[0]);
 	
 	
 	struct Date maturity_date = {15, 12, 2049};
 	struct Date projection_date = {30, 9, 2019};
-
-	
-	
 	
 	periods = monthDiff(projection_date, maturity_date);
 	
@@ -387,11 +487,11 @@ int main()
 	
 	printf("%d\n", periods);
 	
-	float cashflows[1000] = {0};
+	/*float cashflows[1000] = {0};
 	
-	//cashflow_bond(cashflows, scn, 0.053, 2000000.0, 2, periods);
+	cashflow_bond(cashflows, scn, 0.053, 2000000.0, 2, periods);
 	
-	/*for (int i = 0; i < 364; i++)
+	for (int i = 0; i < 364; i++)
 	{
 		printf("%f\n", cashflows[i]);
 	}*/
@@ -400,40 +500,33 @@ int main()
 	
 	printf("mkt value of bond: %f\n", mkt_value);
 	
+	
+	/*
+	
 	struct Date maturity_date1 = {1, 6, 2028};
 	struct Date projection_date1 = {1, 5, 2018};
 
 	
 	periods = monthDiff(projection_date1, maturity_date1);
 	
-
+	*/
 	
-	mkt_value = market_value_commercial_mortgage(0.04524, 3300000.0, 12, 120, 0.0287, 0.0175, 240);
-	printf("mkt value of commercial mtg: %f\n", mkt_value);
+	//mkt_value = market_value_mortgage(0.04524, 3300000.0, 12, 120, 0.0287, 0.0175, 240);
+	//printf("mkt value of commercial mtg: %f\n", mkt_value);
 	
-	float payment = 3300000.0 / ((1.0 - powf( 1.0 / ( 1.0 + 0.04524/12) , 240)) / (0.04524 / 12));
-	printf("payment of commercial mtg: %f\n", payment);
+	//float payment = 3300000.0 / ((1.0 - powf( 1.0 / ( 1.0 + 0.04524/12) , 240)) / (0.04524 / 12));
+	//printf("payment of commercial mtg: %f\n", payment);
+	
+	float mkt_value_cmo = market_value_cmo(0.575, -0.0050, 0.1825, 0.0, 0.0135, 0.0373, 1588473.0, 12, 60, 0.0275, 0.022, 0.0035);
+	
+	//market_value_cmo(float cmoAPortion, float cmoASpread, float cmoBPortion, float cmoBSpread, float cmoCSpread, float underlying_coupon, float underlying_prin, int pmt_freq, int periods,float treasury_rate, float mkt_sprd)
+	printf("mkt value of cmo: %f\n", mkt_value);
 
 }
 
 
 
 
-
-
-/*
-issues:
-dealing with dates in C
-interpolation
-solver functionality
-
-*/
-
-
-
-
-
 /*
 date logic: overiq.com
-
 */
