e16f22d72128c37ea9dcfd57e8f5e367633d99f8
Anthony Voto <88912730+votoaj@users.noreply.github.com>
2022-09-30 16:00:05 -0400

    Update assetmodel.c

--- assetmodel.c
+++ assetmodel.c
@@ -3,24 +3,21 @@
 #include <math.h>
 #include <time.h>
 
-
-/* testing push */
-
 /* GLOBALVARIABLES */
 float yieldcurve_tenors[10] = { 0.25, 0.5, 1, 2, 3, 5, 7, 10, 20, 30 };
 float rates[10] = {0.0172, 0.0251, 0.028, 0.0292, 0.0299, 0.0301, 0.0304, 0.0298, 0.0338, 0.0314};
 int yc_len = 10;
 
-const float call_rates[11] = {-0.05,-0.04,-0.03,-0.02,-0.01,0.0,0.01,0.02,0.03,0.04,0.05}
-const float call_probs[11] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0}
+float call_rates[11] = {-0.05,-0.04,-0.03,-0.02,-0.01,0.0,0.01,0.02,0.03,0.04,0.05};
+float call_probs[11] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0};
 int call_len = 11;
 
-////////////////////
+float rate_data[10][1000][361] = {0.0};
+float cashflows_total[1000][361] = {0.0};
+float market_value_total[1000][361] = {0.0};
 
 
-
-
-
+/////////////////////////////////////
 
 struct Date {
 	int day;
@@ -30,6 +27,9 @@ struct Date {
 
 struct Date _projDate =  {30, 6, 2022};
 
+
+
+//need array of structures to read in all assets
 struct Bond {
 	float coupon;
 	float prin;
@@ -38,39 +38,21 @@ struct Bond {
 	struct Date callDate;	
 	float mkt_spread;
 	float mkt_value_input;
+	float default_rate;
 };
 
+struct Mortgage {
+	float coupon;
+	float prin;
+	int pmt_freq;
+	struct Date maturityDate;
+	float mkt_spread;
+	float mkt_value_input;
+	float default_rate;
+	int amort_term;
+	float service_fee;
+};
 
-
-
-
-
-/*
-float fmax(float x, float y)
-{
-	if (x > y)
-	{
-		return x;
-	} 
-	else
-	{
-		return y;
-	}
-}
-
-
-float fmin(float x, float y)
-{
-	if (x < y)
-	{
-		return x;
-	}
-	else
-	{
-		return y;
-	}
-}
-*/
 
 int monthDiff(struct Date date1, struct Date date2)
 {
@@ -79,6 +61,20 @@ int monthDiff(struct Date date1, struct Date date2)
 	result = date2.month - date1.month + 12 * (date2.year - date1.year);
 	
 	return result;
+}
+
+void set_rates(int scn, int period)
+{
+	rates[0] = rate_data[0][scn][period];
+	rates[1] = rate_data[1][scn][period];
+	rates[2] = rate_data[2][scn][period];
+	rates[3] = rate_data[3][scn][period];
+	rates[4] = rate_data[4][scn][period];
+	rates[5] = rate_data[5][scn][period];
+	rates[6] = rate_data[6][scn][period];
+	rates[7] = rate_data[7][scn][period];
+	rates[8] = rate_data[8][scn][period];
+	rates[9] = rate_data[9][scn][period];
 }
 
 
@@ -125,14 +121,16 @@ float Z(t, T)
 	return exp(-1.0 * r(t, t+T) * (T-t));
 }
 
-
-
-
-float market_value_bond_struct(struct Bond bond1, struct Date projDate) /* pass array or pointer as current yield curve */
+float market_value_bond(struct Bond bond1, struct Date projDate) 
 {
 	float mkt_value = 0.0;
 	float v = 1.0;
 	int periods = monthDiff(projDate, bond1.maturityDate);
+	
+	if (periods <= 0)
+	{
+		return 0.0;
+	}
 	
 	float treasury_rate = r(0.0, periods / 12.0);
 	
@@ -161,6 +159,11 @@ float market_value_bond_struct(struct Bond bond1, struct Date projDate) /* pass 
 	
 }
 
+float psa_model(float factor, float period)
+{
+	return 0.0;
+}
+
 void set_mktSpread_bond(struct Bond *bond1)
 {
 	int itr = 0;
@@ -177,13 +180,13 @@ void set_mktSpread_bond(struct Bond *bond1)
 	{
 		bond1->mkt_spread = a;
 		
-		mkt_diff_a = bond1->mkt_value_input - market_value_bond_struct(*bond1, _projDate);
+		mkt_diff_a = bond1->mkt_value_input - market_value_bond(*bond1, _projDate);
 			
 		//using bisection method here
 		//bisectionMethod(&mkt_spread, a, b, &itr);		
 		bond1->mkt_spread = (a + b) / 2;
 		
-		mkt_diff_mid = bond1->mkt_value_input - market_value_bond_struct(*bond1, _projDate);
+		mkt_diff_mid = bond1->mkt_value_input - market_value_bond(*bond1, _projDate);
 		
 		if (abs(mkt_diff_mid) < allerr)
 		{
@@ -249,55 +252,85 @@ void bisectionMethod(float *x, float a, float b, int *itr)
 }
 
 
-float cashflow_bond(struct Bond bond1, struct Date date1)
+float cashflow_bond(struct Bond *bond1, struct Date date1)
 {
 	
-	float cashflow = 0.0;
-	int periods = monthDiff(projDate, bond1.maturityDate);
-	float treasury_rate = r(0.0, periods / 12.0);
-	float call_margin = 0.01;
+	float interest_paid = 0.0;
+	float prin_paid = 0.0;
 	
-	float call_rate = treasury_rate + bond1.mkt_spread + call_margin - bond1.coupon; // current rate plus margin minus actual coupon
+	int periods = monthDiff(date1, bond1->maturityDate);
+	int periods_to_call = monthDiff(date1, bond1->callDate);
 	
-	float call_prob = naiveInterp(call_rates, call_probs, call_rate, call_len);
-	
-	bond.coupon /= bond.pmt_freq;
-
-	if (periods % (12 / bond.pmt_freq) == 0 )
+	if (periods < 0)
 	{
-		cashflow += bond.prin * bond.coupon;
-		cashflow += bond.prin * call_prob;
+		return 0.0;
+	}
+		
+	float treasury_rate = r(0.0, periods / 12.0);
+	float call_prob = 0.0;
+	
+	if (periods_to_call <= 0)
+	{	
+		float call_margin = 0.01;	
+		float call_rate = bond1->coupon - (treasury_rate + bond1->mkt_spread + call_margin); // current rate plus margin minus actual coupon
+		call_prob = naiveInterp(call_rates, call_probs, call_rate, call_len);
+	}
+
+	
+	bond1->prin *= powf(1.0 - bond1->default_rate, 1.0 / 12.0);
+	
+	float coupon_pmt = bond1->coupon / bond1->pmt_freq;
+
+	if (periods % (12 / bond1->pmt_freq) == 0 )
+	{
+		interest_paid += bond1->prin * coupon_pmt;
+		prin_paid += bond1->prin * call_prob;
 	}
 	
 	if (periods == 0)
 	{
-		cashflow += bond.prin;
+		prin_paid += bond1->prin;
 	}	
 	
-	return cashflow;
+	
+	bond1->prin -= prin_paid;	
+	
+	return prin_paid + interest_paid;
 }
 
 
-float market_value_mortgage(float coupon, float prin, int pmt_freq, int periods,float treasury_rate, float mkt_sprd, int amort_term)
+float market_value_mortgage(struct Mortgage cm1, struct Date projDate)
 {
 	float mkt_value = 0.0;
 	float v = 1.0;
-	float mkt_rate = (treasury_rate + mkt_sprd) / 12.0;
-	coupon /= pmt_freq;
+	
+	int periods = monthDiff(projDate, cm1.maturityDate);	
+	
+	if (periods <= 0)
+	{
+		return 0.0;
+	}
+	
+	float treasury_rate = r(0.0, periods / 12.0);
+	
+	float mkt_rate = (treasury_rate + cm1.mkt_spread) / 12.0;	
+	
+	cm1.coupon /= cm1.pmt_freq;
+		
 	float interest = 0.0;
-	float payment = prin / ((1.0 - powf( 1.0 / ( 1.0 + coupon) , amort_term)) / coupon);
+	float payment = cm1.prin / ((1.0 - powf( 1.0 / ( 1.0 + cm1.coupon) , cm1.amort_term)) / cm1.coupon);
 	
 	while (periods > 0)
 	{
 		
-		if (periods % (12 / pmt_freq) == 0)
+		if (periods % (12 / cm1.pmt_freq) == 0)
 		{
 			
-			interest = prin * coupon;
+			interest = cm1.prin * cm1.coupon;
 			
 			mkt_value += payment * v * powf(1.0 / (1 + mkt_rate), 0.5);  // powf is to account for timings, we assume payments are mid month
 			
-			prin = prin - payment + interest;
+			cm1.prin = cm1.prin - payment + interest;
 		}
 		
 		v /= (1 + mkt_rate);
@@ -306,35 +339,100 @@ float market_value_mortgage(float coupon, float prin, int pmt_freq, int periods,
 	}
 	
 	
-	mkt_value += prin * v * powf(1.0 / (1 + mkt_rate), 0.5);  // any leftover principal.....shouldnt be any
+	mkt_value += cm1.prin * v * powf(1.0 / (1 + mkt_rate), 0.5);  // any leftover principal.....shouldnt be any
 	
 	return mkt_value;
 	
 }
 
 
-float cashflow_mortgage(float scn[361][10], float coupon, float prin, int pmt_freq, int periods, int amort_term)
+float cashflow_mortgage(struct Mortgage *cm1, struct Date projDate)
 {
-	int time_step = 0;
-	coupon /= pmt_freq;
-	float payment = prin / ((1.0 - powf( 1.0 / ( 1.0 + coupon) , amort_term)) / coupon);
+	int periods = monthDiff(projDate, cm1->maturityDate);
+	
+	if (periods < 0)
+	{
+		return 0.0;
+	}	
+	
+	
+	float coupon_pmt = cm1->coupon / cm1->pmt_freq;
+	
+	cm1->prin *= powf(1.0 - cm1->default_rate, 1.0 / 12.0);
+	
+	float payment = cm1->prin / ((1.0 - powf( 1.0 / ( 1.0 + coupon_pmt) , cm1->amort_term)) / coupon_pmt);
+	
+	float interest = 0.0;
+	float fees = 0.0;
 	
 	float cashflow = 0.0;
 		
 
-	if (periods % (12 / pmt_freq) == 0 && periods > 0)
+	if (periods % (12 / cm1->pmt_freq) == 0 && periods > 0)
 	{
 		cashflow = payment;
+		interest = cm1->prin * coupon_pmt;
 	}
 	else if (periods == 0)
 	{
-		cashflow = prin;
+		cashflow = cm1->prin * (1 + coupon_pmt);
+		interest = cm1->prin * coupon_pmt;		
 	}
-
 	
-	return cashflow;
+	fees = cm1->service_fee / cm1->pmt_freq * cm1->prin;
+	
+	cm1->prin = cm1->prin - cashflow + interest;
+	cm1->amort_term -= 1;
+	
+	return cashflow - fees;
 
 }
+
+
+void set_mktSpread_mortgage(struct Mortgage *cm1)
+{
+	int itr = 0;
+	int maxmitr = 40;
+	float allerr = 1.0;
+	float a = 0.0;
+	float b = 0.2;
+	
+	float mkt_diff_a = 0.0;	
+	float mkt_diff_mid = 0.0;
+	
+	
+	while ( itr < maxmitr )
+	{
+		cm1->mkt_spread = a;
+		
+		mkt_diff_a = cm1->mkt_value_input - market_value_mortgage(*cm1, _projDate);
+			
+		//using bisection method here
+		//bisectionMethod(&mkt_spread, a, b, &itr);		
+		cm1->mkt_spread = (a + b) / 2;
+		
+		mkt_diff_mid = cm1->mkt_value_input - market_value_mortgage(*cm1, _projDate);
+		
+		if (abs(mkt_diff_mid) < allerr)
+		{
+			break;
+		}
+		
+		
+		if ((mkt_diff_mid < 0.0 && mkt_diff_a < 0.0) || (mkt_diff_mid > 0.0 && mkt_diff_a > 0.0))
+		{
+			a = cm1->mkt_spread;
+			b = b;
+		}
+		else
+		{
+			a = a;
+			b = cm1->mkt_spread;
+		}			
+		itr++;		
+	}	
+}
+
 
 
 
@@ -410,145 +508,189 @@ float market_value_cmo(float cmoAPortion, float cmoASpread, float cmoBPortion, f
 }
 
 
-
 int main()
 {
-
-	
-	int row = 0;
-	float scn[361][10] = {{0,0}};
-	char line[1000];
-	char* token = NULL;
-	FILE* fp = fopen("C:\\work\\C\\assetmodel\\scn.csv", "r");
-	
-	float temp = 0.0;
 	
 	
+	// read in current bonds
+	struct Bond bonds[500];
+	FILE* bond_input = fopen("C:\\work\\C\\assetmodel\\bonds.csv", "r");
 	
-	
-	
-	
-	for (int i = 0; i < 361; i++)
+	int bond_read = 0;
+	int bond_records = 0;
+	do
 	{
-		int col = 0;
-		
-		while ((fscanf(fp, "%f", &scn[i][col++]) == 1) && col < 10)
+		bond_read = fscanf(bond_input,
+					  "%f,%f,%d,%d,%d,%d,%d,%f,%f,%f\n",
+					  &bonds[bond_records].coupon,
+					  &bonds[bond_records].prin,
+					  &bonds[bond_records].pmt_freq,
+					  &bonds[bond_records].maturityDate.year,
+					  &bonds[bond_records].maturityDate.month,
+					  &bonds[bond_records].callDate.year,
+					  &bonds[bond_records].callDate.month,
+					  &bonds[bond_records].mkt_spread,
+					  &bonds[bond_records].mkt_value_input,
+					  &bonds[bond_records].default_rate);
+		if (bond_read == 10)
 		{
-			fscanf(fp, ",");
+			bond_records++;
+		}
+		
+	} while (!feof(bond_input));
+	
+	fclose(bond_input);
+	
+	// read in commercial mortgages
+	struct Mortgage comm_morts[100];
+	FILE* cm_input = fopen("C:\\work\\C\\assetmodel\\commercial_mortgages.csv", "r");
+	
+	int cm_read = 0;
+	int cm_records = 0;
+	do
+	{
+		cm_read = fscanf(cm_input,
+					  "%f,%f,%d,%d,%d,%f,%f,%f,%d,%f\n",
+					  &comm_morts[cm_records].coupon,
+					  &comm_morts[cm_records].prin,
+					  &comm_morts[cm_records].pmt_freq,
+					  &comm_morts[cm_records].maturityDate.year,
+					  &comm_morts[cm_records].maturityDate.month,
+					  &comm_morts[cm_records].mkt_spread,
+					  &comm_morts[cm_records].mkt_value_input,
+					  &comm_morts[cm_records].default_rate,
+					  &comm_morts[cm_records].amort_term,
+					  &comm_morts[cm_records].service_fee);
+		if (cm_read == 10)
+		{
+			cm_records++;
+		}
+		
+	} while (!feof(cm_input));
+	
+	fclose(cm_input);	
+	
+	FILE* fp = fopen("C:\\work\\C\\assetmodel\\scenarios\\rate_data.csv", "r");
+	
+	
+	for (int r = 0; r < 10; r++)
+	{
+		for (int i = 0; i < 1000; i++)
+		{
+			int col = 0;
+			
+			while ((fscanf(fp, "%f", &rate_data[r][i][col++]) == 1) && col < 361)
+			{
+				fscanf(fp, ",");
+			}
 		}
 	}
 	
-	
-	int periods = 21;
+	fclose(fp);
 
-		
+	
 	struct Date maturity_date = {30, 1, 2032};
 	struct Date projection_date = {30, 6, 2022};
 	
-	
-	periods = monthDiff(projection_date, maturity_date);
+
+
+	float cashflow = 0.0;
 	float mkt_value = 0.0;
 	
-	/*
-	float mkt_value_input = 1997875.0;
-	float mkt_spread = 0.0;
+	fp = fopen("C:\\work\\C\\assetmodel\\output\\cashflows.csv", "w+");
+	FILE* fp2 = fopen("C:\\work\\C\\assetmodel\\output\\marketvalue.csv", "w+");
+	
+	clock_t begin = clock();
 	
 	
-	int itr = 0, maxmitr = 40;
-	float allerr = 0.1;
-	float a = 0.0, b = 0.2;
+	set_rates(0,0);
+	for (int k = 0; k < bond_records; k++)
+	{
+		set_mktSpread_bond(&bonds[k]);
+	}
+	
+	for (int k = 0; k < cm_records; k++)
+	{
+		set_mktSpread_mortgage(&comm_morts[k]);
+	}
 	
 	
-	float mkt_diff_a = 0.0;
 	
-	mkt_value = market_value_bond(0.033, 2500000.0, 2, periods, 0.0, yc_tenors, scn[0]);
-	
-	float mkt_diff_mid = mkt_value_input - mkt_value;
-	
-	
-	while ( fabs(mkt_diff_mid) > allerr && itr < maxmitr )
+	for (int i = 0; i < 1000; i++)
 	{
 		
-		mkt_value = market_value_bond(0.033, 2500000.0, 2, periods, a, yc_tenors, scn[0]);
+		struct Date projection_date = {30, 6, 2022};
+		struct Bond bonds_copy[500];			
+		struct Mortgage cm_copy[100];
 		
-		mkt_diff_a = mkt_value_input - mkt_value;
-		
-		bisectionMethod(&mkt_spread, a, b, &itr);
-		
-		mkt_value = market_value_bond(0.033, 2500000.0, 2, periods, mkt_spread, yc_tenors, scn[0]);
-		
-		mkt_diff_mid = mkt_value_input - mkt_value;
-		
-		if ((mkt_diff_mid < 0.0 && mkt_diff_a < 0.0) || (mkt_diff_mid > 0.0 && mkt_diff_a > 0.0))
+		for (int b = 0; b < bond_records; b++)
 		{
-			a = mkt_spread;
-			b = b;
-		}
-		else
+			bonds_copy[b] = bonds[b];
+			
+		}	
+		
+		
+		for (int b = 0; b < cm_records; b++)
 		{
-			a = a;
-			b = mkt_spread;
+			cm_copy[b] = comm_morts[b];
 		}
+		
+	
+		for (int j = 0; j < 361; j++)
+		{
+			
+
+			set_rates(i,j);
+			
+			float cashflow = 0.0;
+			float mkt_value = 0.0;
+			
+			for (int b = 0; b < bond_records; b++)
+			{
+				mkt_value += market_value_bond(bonds_copy[b], projection_date );
+				cashflow += cashflow_bond(&bonds_copy[b], projection_date);					
+			}	
+			
+			
+			for (int b = 0; b < cm_records; b++)
+			{
+				mkt_value += market_value_mortgage(cm_copy[b], projection_date );
+				cashflow += cashflow_mortgage(&cm_copy[b], projection_date);	
+			}
+			
+			if (projection_date.month == 12)
+			{
+				projection_date.month = 1;
+				projection_date.year += 1;
+			}
+			else
+			{
+				projection_date.month += 1;
+			}
+			
+			fprintf(fp, "%f,", cashflow);
+			fprintf(fp2, "%f,", mkt_value);
+		}
+		
+		fprintf(fp, "\n");
+		fprintf(fp2, "\n");
 		
 		
 	}
 	
-	mkt_value = market_value_bond(0.033, 2500000.0, 2, periods, mkt_spread, yc_tenors, scn[0]);
+	fclose(fp);
+	fclose(fp2);
 	
-	printf("Market Value: %f, Spread: %f\n",mkt_value, mkt_spread);
-	*/
-	struct Bond bond1 = {0.033, 2500000.0, 2, maturity_date, maturity_date, 0.0, 1997875.0};
+	clock_t end = clock();
 	
-	set_mktSpread_bond(&bond1);
-
-	/*
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	
-	float coupon;
-	float prin;
-	int pmt_freq;
-	struct Date maturityDate;
-	struct Date callDate;
-	
-	float mkt_sprd;
-	float periods;
-	
-	market_value_bond_struct(struct Bond bond1, struct Date projDate, float yc_tenors[], float scn[])
-	
-	*/
-	
-	mkt_value = market_value_bond_struct(bond1, projection_date);
-	
-	printf("Market Value: %f, Spread: %f",mkt_value, bond1.mkt_spread);
-	
-	/*
-	float cashInt = 0.0;
-	
-	
-	int time_step = 0;
-	
-	mkt_value = market_value_bond(0.053, 2000000.0, 2, periods, 0.0252, yc_tenors, scn[0]);
-	printf("%f",mkt_value);
-	
-	
-	while (time_step < 361 && periods >= 0)
-	{
-		mkt_value = market_value_bond(0.053, 2000000.0, 2, periods, 0.0252, yc_tenors, scn[time_step]);
-		cashInt = cashflow_bond(scn[time_step], 0.053, 2000000.0, 2, periods);
-		
-		printf("Market Value, Cashflow at time %d: %f, %f\n", time_step, mkt_value, cashInt);
-		
-		periods--;
-		time_step++;
-	
-	}
-
-
-	*/
+	printf("%f\n", time_spent);
 	
 
-	
 	
 }
+
 
 
 
